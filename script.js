a651d0dcc5216fae27110d10c29fe7db41ec6704
Leonardo Alves da Silva <148554458+LeonardoAlvesEtec@users.noreply.github.com>
2025-04-20 19:44:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+
+
+
+import EntregasController from './controller/entregasController.js';
+
+
+const apiKey = 'tl9o9OWNAGSOewQ1PHhdFGWRctiMrwDa';
+
+var map = tt.map({
+  key: "tl9o9OWNAGSOewQ1PHhdFGWRctiMrwDa",
+  container: "mapa",
+  zoom: 15
+})
+
+      const entregasController = new EntregasController(map);
+      entregasController.startSimulation();
+
+
+
+
+      
+      function startTimer(duration, display) {
+
+        var timer = duration, minutes, seconds;
+        
+
+
+         setInterval(function () {
+        
+         minutes = parseInt(timer / 60, 10)
+        
+         seconds = parseInt(timer % 60, 10);
+
+ minutes = minutes < 10 ? "0" + minutes : minutes;
+
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+         display.textContent = minutes + ":" + seconds;
+
+         if (--timer < 0) { timer = 0;} }, 1000);}
+
+        window.onload = function () {
+        
+         var time = 60 / 2, 
+        
+         display = document.querySelector('#safeTimerDisplay');
+        
+         startTimer(time, display);
+        
+        };
+
+        exports.handler = async (event, context) => {
+          // Lógica de autenticação aqui
+          return {
+            statusCode: 200,
+            body: JSON.stringify({ message: "Usuário autenticado!" }),
+          };
+        };
